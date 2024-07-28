bb3514bd32312bbc358d429e03f96134fe2fc0f0
syamsaSJ <julyindasyamsa@gmail.com>
2024-07-28 21:42:42 +0700

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <nav className='bg-slate-400'>
+        <div>
+            logo
+        </div>
+        <div>
+           
+        </div>
+
+    </nav>
+  );
+};
+
+export default Home;
